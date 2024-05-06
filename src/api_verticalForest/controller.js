4a384a5c20c9409673f34445acd0d7c4aee7d294
planetcentriclab <planetcentriclab@gmail.com>
2024-05-06 12:15:00 +0700

    Edit script of controller.js of vertical forest project

--- src/api_verticalForest/controller.js
+++ src/api_verticalForest/controller.js
@@ -38,8 +38,14 @@ const select_soilPH = (req, res) => {
         res.status(200).json(result.rows)
     })
 }
-const select_waterFlow = (req, res) => {
-    pool.query(querise.select_waterFlow , (error, result) => {
+const select_waterFlow1 = (req, res) => {
+    pool.query(querise.select_waterFlow1 , (error, result) => {
+        if (error) throw error;
+        res.status(200).json(result.rows)
+    })
+}
+const select_waterFlow2 = (req, res) => {
+    pool.query(querise.select_waterFlow2 , (error, result) => {
         if (error) throw error;
         res.status(200).json(result.rows)
     })
@@ -66,13 +72,17 @@ const newRecord = (req, res) => {
         soilTemperature, 
         soilMoisture,
         soilPH,
-        waterFlow,
+        waterFlow1,
+        waterFlow2,
         voltageFlow,
         currentFlow
     } = req.body
 
     const create_date = new Date().toISOString().slice(0, 10)
-    const create_time = new Date().toLocaleTimeString('en-US', { timeZone: 'Asia/Bangkok', hour12: false })
+    const create_time = new Date().toLocaleTimeString('en-US', {timeZone: 'Asia/Bangkok', hour12: false})
+    if (create_time.startsWith("24")) {
+        create_time = create_time.replace(/^24/, "00");
+    }
 
     pool.query(querise.newRecord_lightIntensity, [create_date, create_time, lightIntensity.value_raw, lightIntensity.value_con])
     pool.query(querise.newRecord_temperature, [create_date, create_time, temperature.value_raw, temperature.value_con])
@@ -80,7 +90,8 @@ const newRecord = (req, res) => {
     pool.query(querise.newRecord_soilTemperature, [create_date, create_time, soilTemperature.value_raw, soilTemperature.value_con])
     pool.query(querise.newRecord_soilMoisture, [create_date, create_time, soilMoisture.value_raw, soilMoisture.value_con])
     pool.query(querise.newRecord_soilPH, [create_date, create_time, soilPH.value_raw, soilPH.value_con])
-    pool.query(querise.newRecord_waterFlow, [create_date, create_time, waterFlow.value_raw, waterFlow.value_con])
+    pool.query(querise.newRecord_waterFlow1, [create_date, create_time, waterFlow1.value_raw, waterFlow1.value_con])
+    pool.query(querise.newRecord_waterFlow2, [create_date, create_time, waterFlow2.value_raw, waterFlow2.value_con])
     pool.query(querise.newRecord_voltageFlow, [create_date, create_time, voltageFlow.value_raw, voltageFlow.value_con])
     pool.query(querise.newRecord_currentFlow, [create_date, create_time, currentFlow.value_raw, currentFlow.value_con])
 
@@ -94,7 +105,8 @@ module.exports = {
     select_soilTemperature,
     select_soilMoisture,
     select_soilPH,
-    select_waterFlow,
+    select_waterFlow1,
+    select_waterFlow2,
     select_voltageFlow,
     select_currentFlow,
 
