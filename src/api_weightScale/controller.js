626ad0fb89d8ecaa83df7e7cf48838a75ff93304
planetcentriclab <planetcentriclab@gmail.com>
2024-05-06 12:17:08 +0700

    Edit script of controller.js of weight waste project

--- src/api_weightScale/controller.js
+++ src/api_weightScale/controller.js
@@ -35,7 +35,7 @@ const selectAll_machine_forDashboard = (req, res) => {
             const thaiYear = date.getFullYear() + 543;
             const thaiMonth = (date.getMonth() + 1).toString().padStart(2, '0');
             const thaiDay = date.getDate().toString().padStart(2, '0');
-            return `${thaiDay}/${thaiMonth}/${thaiYear.toString().slice(2)}`;
+            return `${thaiDay}/${thaiMonth}/${thaiYear.toString().slice()}`;
         }
         const modifiedDate = data.map(item => {
             return { ...item, create_date: convertToThaiDate(item.create_date) };
@@ -56,6 +56,9 @@ const newMachine = (req, res) => {
     const {faculty, machine_name, all_profile} = req.body
     const create_date = new Date().toISOString().slice(0, 10)
     const create_time = new Date().toLocaleTimeString('en-US', {timeZone: 'Asia/Bangkok', hour12: false})
+    if (create_time.startsWith("24")) {
+        create_time = create_time.replace(/^24/, "00");
+    }
 
     //*------------ < check if Machine_name exists >
     pool.query(querise.checkMachine_nameExists, [machine_name], (error, result) => {
@@ -75,26 +78,54 @@ const newMachine = (req, res) => {
 }
 
 const editMachine = (req, res) => {
-    const {machine_name, faculty, all_profile} = req.body
+    const {machine_name, machine_name_new, faculty, all_profile} = req.body
     console.log(req.body)
-    //*------------ < check if Machine_name exists >
-    pool.query(querise.checkMachine_nameExists, [machine_name], (error, result) => {
-        if(result.rows.length){
-            //*------------ < add new machine  >
-            pool.query(querise.editMachine, [machine_name, faculty, all_profile], (error, result) => {
-                if (error) throw error;
-                res.status(201).send("Edited machine successfully.!")
-            })
-        }
-        else{
-            res.send("This Machine_name not found~!")
-        }
-    })
+
+    if(machine_name === machine_name_new){  //Todo: ========= don't change machine name
+        // console.log("don't change");
+        pool.query(querise.checkMachine_nameExists, [machine_name], (error, result) => {
+            if(result.rows.length){ //Todo: ========= machine name already exist
+                pool.query(querise.editMachine, [machine_name, machine_name_new, faculty, all_profile], (error, result) => {
+                    if (error) throw error;
+                    // res.status(201).send("Edited machine successfully.!")
+                    res.status(201).send("พบเครื่องที่ต้องการ และแก้ไขข้อมูลต่างๆให้แล้ว")
+                })
+            }
+            else{   //Todo: ========= machine name not found
+                // res.send("This Machine_name not found~!")
+                res.send("ไม่พบเครื่องที่ต้องการแก้ไขข้อมูล")
+            }
+        })
+    }
+    else{   //Todo: ========= change machine name
+        // console.log("change");
+        pool.query(querise.checkMachine_nameExists, [machine_name], (error, result) => {
+            if(result.rows.length){ //Todo: ========= machine name already exist
+
+                pool.query(querise.checkMachine_nameExists, [machine_name_new], (error, result) => {
+                    if(result.rows.length){ //Todo: ========= new machine name is already exist
+                        res.send("พบเครื่องที่มีชื่อซ้ำแล้ว ใช้ชื่อนี้ไม่ได้นะจ๊ะ")
+                    }
+                    else{
+                        pool.query(querise.editMachine, [machine_name, machine_name_new, faculty, all_profile], (error, result) => {
+                            if (error) throw error;
+                            // res.status(201).send("Edited machine successfully.!")
+                            res.status(201).send(`พบเครื่องที่ต้องการ และแก้ไขชื่อจาก ${machine_name} เป็น ${machine_name_new} กับข้อมูลต่างๆให้แล้ว :)`)
+                        })
+                    }
+                })
+            }
+            else{   //Todo: ========= machine name not found
+                // res.send("This Machine_name not found~!")
+                res.send("ไม่พบเครื่องที่ต้องการเปลี่ยนชื่อและแก้ไขข้อมูล")
+            }
+        })
+    }
 }
 
 const deleteMachine = (req, res) => {
     const {machine_name} = req.body
-    console.log(req.body)
+    // console.log(req.body)
 
     //*------------ < check if Machine_name exists >
     pool.query(querise.checkMachine_nameExists, [machine_name], (error, result) => {
@@ -152,93 +183,164 @@ const summary = (req, res) => {
     const {profile_waste, faculty, start_date, end_date} = req.body
     // console.log(req.body)
 
-    pool.query(querise.selectFor_summary, [profile_waste, faculty, start_date, end_date], (error, result) =>{
+    pool.query(querise.selectAll_profile_of_waste_thai , (error, result) => {
         if (error) throw error;
-        // console.log(result.rows);
+        console.log(result.rows);
+        const profile_of_waste = result.rows
 
-        function calculateWasteAmount(data){
-            const wasteMap = {}
-            const carbonFactor = {
-                'กระป๋อง': 2,
-                'ถุงวิบวับ': 2,
-                'เศษอาหาร': 2,
-                'แก้ว': 2,
-                'ขยะอันตราย': 2,
-                'กล่องนม': 2,
-                'ขยะทั่วไป': 2,
-                'ขยะกำพร้า': 2,
-                'กระดาษ': 2,
-                'ถุงพลาสติก': 2,
-                'ขวดพลาสติก': 2,
-                'กล่องอาหารพลาสติก': 2,
-                'แก้วน้ำพลาสติก(ขายไม่ได้)': 2,
-                'แก้วน้ำพลาสติก(ขายได้)': 2,
-                'ช้อนส้อมพลาสติก': 2,
-                'หลอดพลาสติก': 2,
-                'ขยะห้องน้ำ': 2
-            }
+        pool.query(querise.selectFor_summary, [profile_waste, faculty, start_date, end_date], (error, result) =>{
+            if (error) throw error;
+            console.log(result.rows);
 
-            let total_waste_detail;
-            let total_carbon_detail;
-            let amount_waste_detail;
-            let amount_carbon_detail;
-            let first_create_date;
-
-            if(Array.isArray(data) && data.length === 0){
-                total_waste_detail = 0
-                total_carbon_detail = 0
-                amount_waste_detail = 0
-                amount_carbon_detail = 0
-                first_create_date = 0   
-            }
-            else{
-                data.forEach(item => {
+            function calculateWasteAmount(data1, data2) {
+                const wasteMap = {};
+                const carbonFactors = {
+                    'กระป๋อง': 2,
+                    'ถุงวิบวับ': 2,
+                    'เศษอาหาร': 2,
+                    'แก้ว': 2,
+                    'ขยะอันตราย': 2,
+                    'กล่องนม': 2,
+                    'ขยะทั่วไป': 2,
+                    'ขยะกำพร้า': 2,
+                    'กระดาษ': 2,
+                    'ถุงพลาสติก': 2,
+                    'ขวดพลาสติก': 2,
+                    'กล่องอาหารพลาสติก': 2,
+                    'แก้วน้ำพลาสติก(ขายไม่ได้)': 2,
+                    'แก้วน้ำพลาสติก(ขายได้)': 2,
+                    'ช้อนส้อมพลาสติก': 2,
+                    'หลอดพลาสติก': 2,
+                    'ขยะห้องน้ำ': 2
+                };
+        
+                data1.forEach(item => {
                     const profile = item.waste_profile;
                     const weight = parseFloat(item.weight);
                     if (wasteMap[profile]) {
                         wasteMap[profile] += weight;
-                    } 
-                    else {
+                    } else {
                         wasteMap[profile] = weight;
                     }
                 });
-    
-                amount_waste_detail = Object.keys(wasteMap).map(profile => {
-                    return {
-                        profile: profile,
-                        amount: wasteMap[profile]
-                    };
+        
+                const amount_waste_detail = data2.map(item => {
+                    return { profile: item.waste_profile_thai, amount: parseFloat((wasteMap[item.waste_profile_thai] || 0).toFixed(2))};
                 });
-    
-                amount_carbon_detail = Object.keys(wasteMap).map(profile => {
-                    const amount = wasteMap[profile];
-                    const factor = carbonFactor[profile];
-                    return {
-                        profile: profile,
-                        amount: amount * factor
-                    };
+            
+                const amount_carbon_detail = data2.map(item => {
+                    const profile = item.waste_profile_thai;
+                    const factor = carbonFactors[profile] || 1;
+                    const amount = wasteMap[profile] || 0;
+                    return { profile, amount: parseFloat((amount * factor).toFixed(2)) };
                 });
-    
-                total_waste_detail = amount_waste_detail.reduce((total, item) => total + item.amount, 0)
-    
-                total_carbon_detail = amount_carbon_detail.reduce((total, item) => total + item.amount, 0)
+                
+                const total_waste_detail = parseFloat((amount_waste_detail.reduce((total, item) => total + item.amount, 0)).toFixed(2))
 
-                first_create_date = data[0].create_date
+                const total_carbon_detail = parseFloat((amount_carbon_detail.reduce((total, item) => total + item.amount, 0)).toFixed(2))
+        
+                if(Array.isArray(data1) && data1.length === 0){
+                    first_create_date = 0
+                }
+                else{
+                    first_create_date = data1[0].create_date
+                }
+            
+                return { total_waste_detail, total_carbon_detail, amount_waste_detail, amount_carbon_detail, first_create_date};
             }
-
-            return {total_waste_detail, total_carbon_detail, amount_waste_detail, amount_carbon_detail, first_create_date}
-        }
-
-        const jsonData = calculateWasteAmount(result.rows)
-        // console.log(jsonData)
-        res.status(200).json(jsonData)
+            const jsonData = calculateWasteAmount(result.rows, profile_of_waste)
+            // console.log(jsonData)
+            res.status(200).json(jsonData)
+        })
     })
 }
+        // *---------------------------------------------------------
+    //     function calculateWasteAmount(data){
+    //         const wasteMap = {}
+    //         const carbonFactor = {
+    //             'กระป๋อง': 2,
+    //             'ถุงวิบวับ': 2,
+    //             'เศษอาหาร': 2,
+    //             'แก้ว': 2,
+    //             'ขยะอันตราย': 2,
+    //             'กล่องนม': 2,
+    //             'ขยะทั่วไป': 2,
+    //             'ขยะกำพร้า': 2,
+    //             'กระดาษ': 2,
+    //             'ถุงพลาสติก': 2,
+    //             'ขวดพลาสติก': 2,
+    //             'กล่องอาหารพลาสติก': 2,
+    //             'แก้วน้ำพลาสติก(ขายไม่ได้)': 2,
+    //             'แก้วน้ำพลาสติก(ขายได้)': 2,
+    //             'ช้อนส้อมพลาสติก': 2,
+    //             'หลอดพลาสติก': 2,
+    //             'ขยะห้องน้ำ': 2
+    //         }
+
+    //         let total_waste_detail;
+    //         let total_carbon_detail;
+    //         let amount_waste_detail;
+    //         let amount_carbon_detail;
+    //         let first_create_date;
+
+    //         if(Array.isArray(data) && data.length === 0){
+    //             total_waste_detail = 0
+    //             total_carbon_detail = 0
+    //             amount_waste_detail = 0
+    //             amount_carbon_detail = 0
+    //             first_create_date = 0   
+    //         }
+    //         else{
+    //             data.forEach(item => {
+    //                 const profile = item.waste_profile;
+    //                 const weight = parseFloat(item.weight);
+    //                 if (wasteMap[profile]) {
+    //                     wasteMap[profile] += weight;
+    //                 } 
+    //                 else {
+    //                     wasteMap[profile] = weight;
+    //                 }
+    //             });
+    
+    //             amount_waste_detail = Object.keys(wasteMap).map(profile => {
+    //                 return {
+    //                     profile: profile,
+    //                     amount: parseFloat((wasteMap[profile]).toFixed(2))
+    //                 };
+    //             });
+    
+    //             amount_carbon_detail = Object.keys(wasteMap).map(profile => {
+    //                 const amount = wasteMap[profile];
+    //                 const factor = carbonFactor[profile];
+    //                 return {
+    //                     profile: profile,
+    //                     amount: parseFloat((amount * factor).toFixed(2))
+    //                 };
+    //             });
+    
+    //             total_waste_detail = parseFloat((amount_waste_detail.reduce((total, item) => total + item.amount, 0)).toFixed(2))
+    
+    //             total_carbon_detail = parseFloat((amount_carbon_detail.reduce((total, item) => total + item.amount, 0)).toFixed(2))
+
+    //             first_create_date = data[0].create_date
+    //         }
+
+    //         return {total_waste_detail, total_carbon_detail, amount_waste_detail, amount_carbon_detail, first_create_date}
+    //     }
+
+    //     const jsonData = calculateWasteAmount(result.rows)
+    //     // console.log(jsonData)
+    //     res.status(200).json(jsonData)
+//     })
+// }
 
 const newRecord = (req, res) => {
     const {faculty, machine_name, waste_profile, weight} = req.body
     const create_date = new Date().toISOString().slice(0, 10)
     const create_time = new Date().toLocaleTimeString('en-US', {timeZone: 'Asia/Bangkok', hour12: false})
+    if (create_time.startsWith("24")) {
+        create_time = create_time.replace(/^24/, "00");
+    }
 
     const UUID = crypto.randomUUID()
     pool.query(querise.newRecord, [UUID, create_date, faculty, machine_name, waste_profile, weight, create_time], (error, result) => {
