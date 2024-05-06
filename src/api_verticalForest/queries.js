4dec21ced1b4b6ff7ab737ff5cbf7e20e35c07a2
planetcentriclab <planetcentriclab@gmail.com>
2024-05-06 12:15:26 +0700

    Edit script of queries.js of vertical forest project

--- src/api_verticalForest/queries.js
+++ src/api_verticalForest/queries.js
@@ -5,7 +5,8 @@ const select_humidity = `Select * from "humidity"`
 const select_soilTemperature = `Select * from "soilTemperature"`
 const select_soilMoisture = `Select * from "soilMoisture"`
 const select_soilPH = `Select * from "soilPH"`
-const select_waterFlow = `Select * from "waterFlow"`
+const select_waterFlow1 = `Select * from "waterFlow1"`
+const select_waterFlow2 = `Select * from "waterFlow2"`
 const select_voltageFlow = `Select * from "voltageFlow"`
 const select_currentFlow = `Select * from "currentFlow"`
 
@@ -15,7 +16,8 @@ const newRecord_humidity = `insert into "humidity" (date, time, value_raw, value
 const newRecord_soilTemperature = `insert into "soilTemperature" (date, time, value_raw, value_con) values($1, $2, $3, $4)`
 const newRecord_soilMoisture = `insert into "soilMoisture" (date, time, value_raw, value_con) values($1, $2, $3, $4)`
 const newRecord_soilPH = `insert into "soilPH" (date, time, value_raw, value_con) values($1, $2, $3, $4)`
-const newRecord_waterFlow = `insert into "waterFlow" (date, time, value_raw, value_con) values($1, $2, $3, $4)`
+const newRecord_waterFlow1 = `insert into "waterFlow1" (date, time, value_raw, value_con) values($1, $2, $3, $4)`
+const newRecord_waterFlow2 = `insert into "waterFlow2" (date, time, value_raw, value_con) values($1, $2, $3, $4)`
 const newRecord_voltageFlow = `insert into "voltageFlow" (date, time, value_raw, value_con) values($1, $2, $3, $4)`
 const newRecord_currentFlow = `insert into "currentFlow" (date, time, value_raw, value_con) values($1, $2, $3, $4)`
 
@@ -28,7 +30,8 @@ module.exports = {
     select_soilTemperature,
     select_soilMoisture,
     select_soilPH,
-    select_waterFlow,
+    select_waterFlow1,
+    select_waterFlow2,
     select_voltageFlow,
     select_currentFlow,
 
@@ -38,7 +41,8 @@ module.exports = {
     newRecord_soilTemperature,
     newRecord_soilMoisture,
     newRecord_soilPH,
-    newRecord_waterFlow,
+    newRecord_waterFlow1,
+    newRecord_waterFlow2,
     newRecord_voltageFlow,
     newRecord_currentFlow
 
